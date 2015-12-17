996ed8d9e460b13306e205fd12395f01d3f6417b
Björn Albers <bjoernalbers@gmail.com>
2015-12-17 13:18:50 +0100

    Add navbar and fancy flash messages.

---
+++ app/helpers/bootstrap_flash_helper.rb
@@ -0,0 +1,24 @@
+module BootstrapFlashHelper
+  ALERT_TYPES = [:success, :info, :warning, :danger] unless const_defined?(:ALERT_TYPES)
+
+  def bootstrap_flash(options = {})
+    flash_messages = []
+    flash.each do |type, message|
+      next if message.blank?
+
+      type = type.to_sym
+      type = :success if type == :notice
+      type = :danger  if type == :alert
+      type = :danger  if type == :error
+      next unless ALERT_TYPES.include?(type)
+
+      Array(message).each do |msg|
+        text = content_tag(:div,
+                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+                           msg, :class => "alert fade in alert-#{type} #{options[:class]}")
+        flash_messages << text if msg
+      end
+    end
+    flash_messages.join("\n").html_safe
+  end
+end
