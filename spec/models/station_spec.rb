c9857315a778eb5e7fe38fdec1a7f0bd9840d571
Björn Albers <bjoernalbers@gmail.com>
2016-07-14 12:26:41 +0200

    Refactor station model spec.

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -7,69 +7,69 @@ describe Station do
   end
 
   it 'has many orders' do
-    station = create(:station)
-    order = create(:order, station: station)
-    expect(station.orders).to match_array [order]
+    subject = create(:station)
+    order = create(:order, station: subject)
+    expect(subject.orders).to match_array [order]
   end
 
   context 'when destroyed' do
-    let(:station) { create(:station) }
+    let(:subject) { create(:station) }
 
     it 'deletes depenend orders' do
-      order = create(:order, station: station)
-      expect{ station.destroy }.to change(Order, :count).by(-1)
+      order = create(:order, station: subject)
+      expect{ subject.destroy }.to change(Order, :count).by(-1)
     end
   end
 
   describe '#name' do
     it 'must be present' do
-      station = build(:station, name: nil)
-      expect(station).to be_invalid
-      expect(station.errors[:name]).to be_present
-      expect{ station.save!(validate: false) }.to raise_error
+      subject = build(:station, name: nil)
+      expect(subject).to be_invalid
+      expect(subject.errors[:name]).to be_present
+      expect{ subject.save!(validate: false) }.to raise_error
     end
   end
 
   describe '#modality' do
     it 'must be present' do
-      station = build(:station, modality: nil)
-      expect(station).to be_invalid
-      expect(station.errors[:modality]).to be_present
-      expect{ station.save!(validate: false) }.to raise_error
+      subject = build(:station, modality: nil)
+      expect(subject).to be_invalid
+      expect(subject.errors[:modality]).to be_present
+      expect{ subject.save!(validate: false) }.to raise_error
     end
 
     it 'must include valid code' do
       valid_modality_codes = %w{CR CT DX MG MR NM US}
       valid_modality_codes.each do |code|
-        station = build(:station, modality: code)
-        expect(station).to be_valid
+        subject = build(:station, modality: code)
+        expect(subject).to be_valid
       end
 
       invalid_modality_codes = %w{AB Cr cT dx}
       invalid_modality_codes += ['', 'CT.', ' MR', "NM\t", 'ÜS']
       invalid_modality_codes.each do |code|
-        station = build(:station, modality: code)
-        expect(station).to be_invalid
-        expect(station.errors[:modality]).to be_present
+        subject = build(:station, modality: code)
+        expect(subject).to be_invalid
+        expect(subject.errors[:modality]).to be_present
       end
     end
   end
 
   describe '#aetitle' do
     it 'must be present' do
-      station = build(:station, aetitle: nil)
-      expect(station).to be_invalid
-      expect(station.errors[:aetitle]).to be_present
-      expect{ station.save!(validate: false) }.to raise_error
+      subject = build(:station, aetitle: nil)
+      expect(subject).to be_invalid
+      expect(subject.errors[:aetitle]).to be_present
+      expect{ subject.save!(validate: false) }.to raise_error
     end
 
     it 'must include 16 characters max' do
-      station = build(:station, aetitle: 'A'*16)
-      expect(station).to be_valid
+      subject = build(:station, aetitle: 'A'*16)
+      expect(subject).to be_valid
 
-      station = build(:station, aetitle: 'A'*17)
-      expect(station).to be_invalid
-      expect(station.errors[:aetitle]).to be_present
+      subject = build(:station, aetitle: 'A'*17)
+      expect(subject).to be_invalid
+      expect(subject.errors[:aetitle]).to be_present
     end
 
     it 'must include only characters, digits, underscores, dashes and dots' do
@@ -81,8 +81,8 @@ describe Station do
         'Philips'
       ]
       valid_aetitles.each do |aetitle|
-        station = build(:station, aetitle: aetitle)
-        expect(station).to be_valid
+        subject = build(:station, aetitle: aetitle)
+        expect(subject).to be_valid
       end
 
       invalid_aetitles = [
@@ -90,18 +90,18 @@ describe Station do
         'PHILIPS,1'
       ]
       invalid_aetitles.each do |aetitle|
-        station = build(:station, aetitle: aetitle)
-        expect(station).to be_invalid
-        expect(station.errors[:aetitle]).to be_present
+        subject = build(:station, aetitle: aetitle)
+        expect(subject).to be_invalid
+        expect(subject.errors[:aetitle]).to be_present
       end
     end
   end
 
   describe 'receives_orders_via_hl7' do
-    let(:station) { build(:station) }
+    let(:subject) { build(:station) }
 
     it 'defaults to false' do
-      expect(station.receives_orders_via_hl7).to be_falsey
+      expect(subject.receives_orders_via_hl7).to be_falsey
     end
   end
 end
