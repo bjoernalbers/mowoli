7a9a21ab7b8038e4a6d34f74366603831fda00d0
Björn Albers <bjoernalbers@gmail.com>
2022-01-31 10:44:36 +0100

    Log to STDOUT in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,9 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  # Log to STDOUT
+  STDOUT.sync = true
+  config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
